a695839c5b59aa40693b5e46e190444074507b5c
SUBHADIP SINHA <119493894+subhadipsinha722133@users.noreply.github.com>
2024-09-19 22:25:38 +0530

    Create 2.py

---
+++ 2.py
@@ -0,0 +1,12 @@
+import seaborn as sns
+
+import matplotlib.pyplot as plt
+import pandas as pd
+
+
+var = sns.load_dataset("penguins")
+print(var)
+
+
+sns.displot(var["flipper_length_mm"], bins=[170, 180, 190, 200, 210, 220, 230, 240])
+plt.show()
